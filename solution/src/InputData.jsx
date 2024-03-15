3cc08a05b27df0dc5a307b5a21282a1815df625a
AustinUiUx <AustinUiUx@gmail.com>
2024-03-14 21:13:01 -0700

    2nd commit with comments.

--- solution/src/InputData.jsx
+++ solution/src/InputData.jsx
@@ -9,8 +9,13 @@ export default function InputData() {
     const [submissions, setSubmissions] = useState([]);
 
     useEffect(() => {
-        getLocations().then(setLocations);
+        getLocations()
+            .then(setLocations);
     }, []);
+    //using use effect to ensure I only fetch the data once and not on a loop
+    //instead of using the standard fetch().then(res => res.json()).then(data => data.)
+    //I went with just using getLocations expecting a response i used setLocations which is a function that gets all locations
+
 
     const handleSubmit = () => {
         setSubmissions([...submissions, { name, location: selectedLocation }]);
@@ -18,16 +23,21 @@ export default function InputData() {
         setSelectedLocation('');
     };
 
+    //I am using setSubmissions to get the array of submissions but i want to spread the submissions
+    //This will give me a new array and I will not effect the exisiting array thanks to the spread operator
+    //handleSubmit is a functions that that is listening for a click, it then takes the value of name and location into
+    //a div.
+
 
     return (
-        <div style={{display: 'flex', flexDirection: "column"}}>
-            <label htmlFor="nameInput" style={{marginRight: '10px', color: 'black'}}>Name:</label>
+        <div style={{display: 'flex', flexDirection: "column", color: "black"}}>
+            <label htmlFor="nameInput" style={{marginRight: '10px'}}>Name:</label>
             <input
                 type="text"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
             />
-            <label htmlFor="nameInput" style={{marginRight: '10px', color: 'black'}}>Location:</label>
+            <label htmlFor="nameInput" style={{marginRight: '10px',}}>Location:</label>
             <select
                 value={selectedLocation}
                 onChange={(e) => setSelectedLocation(e.target.value)}
@@ -38,9 +48,12 @@ export default function InputData() {
                     </option>
                 ))}
             </select>
+            <div style={{alignSelf: "flex-end"}}>
             <button onClick={handleSubmit}>Submit</button>
             <button onClick={() => setSubmissions([])}>Clear Submissions</button>
-            <div style={{display: 'flex', marginTop: '20px', color: "black"}}>
+            </div>
+            {/*this onClick is taking the function setSubmissions and clearing out the array so its empty*/}
+            <div style={{display: 'flex', marginTop: '20px'}}>
                 <div style={{marginRight: '20px'}}>
                     <strong>Names</strong>
                     {submissions.map((submission, index) => (
